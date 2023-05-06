e98319dda13c6264bb88b79b4a33184c1765c014
mushfiq <mushfiqxrabbi@gmail.com>
2023-05-06 21:16:38 +0600

    add credit

--- components/IntroSection.js
+++ components/IntroSection.js
@@ -12,16 +12,19 @@ export default function IntroSection() {
           Hi, I'm Mushfiq. Nice to meet you.
         </Heading>
       </header>
-      <article className="lg:container mx-auto lg:max-w-6xl">
+      <article className="lg:container mx-auto lg:max-w-5xl">
         <BodyText
           value={2}
-          className="md:px-28 md:text-xl text-slate-200 lg:mt-8"
+          className="md:px-28 md:text-xl text-slate-200 lg:mt-8 lg:text-xl"
         >
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aut fuga
-          fugit quo cumque molestias, distinctio illo molestiae. Praesentium
-          ratione animi distinctio culpa hic cum sit quia molestias ex assumenda
-          minima possimus modi ea necessitatibus laborum, cupiditate voluptate?
-          Facere, in sapiente!
+          I am a full-stack web developer with a degree in computer science. I
+          specialize in using MongoDB, Express.js, React.js, and Node.js to
+          create scalable and efficient full-stack applications. I have a
+          passion for creating visually appealing and dynamic user interfaces
+          using React.js. With my knowledge of back-end development using
+          Node.js and Express.js, I can build robust server-side logic and APIs.
+          I am constantly learning and keeping up-to-date with the latest trends
+          in web development.
         </BodyText>
       </article>
     </section>
