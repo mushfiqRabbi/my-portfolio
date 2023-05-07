13d1f20aa558915dcf08c81ad197d61b1ce57934
mushfiq <mushfiqxrabbi@gmail.com>
2023-05-07 08:35:21 +0600

    almost done

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,14 @@
 import "@/styles/globals.css";
-import { Inter, Reem_Kufi, Comme, Ysabeau } from "next/font/google";
+// import { Inter, Reem_Kufi, Comme, Ysabeau } from "next/font/google";
 
-const inter = Inter({ subsets: ["latin"] });
-const reemKufi = Reem_Kufi({ subsets: ["latin"] });
-const comme = Comme({ subsets: ["latin"] });
-const ysabeau = Ysabeau({ subsets: ["latin"] });
+// const inter = Inter({ subsets: ["latin"] });
+// const reemKufi = Reem_Kufi({ subsets: ["latin"] });
+// const comme = Comme({ subsets: ["latin"] });
+// const ysabeau = Ysabeau({ subsets: ["latin"] });
 
 export default function App({ Component, pageProps }) {
   return (
-    <main className={`${comme.className}`}>
+    <main>
       <Component {...pageProps} />
     </main>
   );
